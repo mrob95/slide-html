f3fc6de1f6ec784f1d24c4586fdc4859cbfb7452
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-07-31 20:41:16 +0100

    Minor adjustments

--- bin/slide.py
+++ bin/slide.py
@@ -5,22 +5,21 @@ call_path = os.getcwd()
 
 def to_bytes(image_path):
     if image_path[:4] == "http":
-        return base64.b64encode(requests.get(image_path).content).decode("utf-8")
+        image = requests.get(image_path).content
     else:
         with open(image_path, "rb") as f:
-            return base64.b64encode(f.read()).decode("utf-8")
+            image = f.read()
+    return base64.b64encode(image).decode("utf-8")
 
 def img_type(image_path):
     extension = image_path.rsplit(".", 1)[1]
-    if extension in ["jpeg", "jpg"]:
-        return "jpeg"
-    elif extension == "png":
-        return "png"
-    elif extension == "gif":
-        return "gif"
-    elif extension == "bmp":
-        return "bmp"
+    if extension in ["jpeg", "jpg"]: return "jpeg"
+    elif extension == "png": return "png"
+    elif extension == "gif": return "gif"
+    elif extension == "bmp": return "bmp"
+    elif extension == "svg": return "svg"
 
+# Replaces images and adds tabs
 def format_lines(input_lines):
     output = []
     for line in input_lines:
@@ -28,17 +27,17 @@ def format_lines(input_lines):
             image_path = line[1:]
             image_type = img_type(image_path.strip())
             image_text = to_bytes(image_path.strip())
-            line = "<img src=\"data:image/%s;base64,%s\">\n" % (image_type, image_text)
+            line = '<img src="data:image/%s;base64,%s">\n' % (image_type, image_text)
         output.append("\t\t\t" + line)
     return output
 
 def main():
     with open(sys.argv[1], "r") as input:
         formatted = "".join(format_lines(input.readlines()))
-    with open(os.path.join(source_path, "slide.html"), "r") as template:
-        output = template.read().replace("{{ CONTENT }}", formatted)
-    with open(sys.argv[2], "w+") as result:
-        result.write(output)
+    with open(os.path.join(source_path, "template.html"), "r") as template:
+        result = template.read().replace("{{ CONTENT }}", formatted)
+    with open(sys.argv[2], "w+") as output:
+        output.write(result)
 
 if __name__ == '__main__':
     main()
